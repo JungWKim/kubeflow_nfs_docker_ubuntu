b8f5d9d0d7c1a686946c86bc2fbab2baed760ccd
JungWKim <lionmini@naver.com>
2023-10-03 00:05:14 +0900

    .

--- setup_kubeflow.sh
+++ setup_kubeflow.sh
@@ -9,10 +9,42 @@ sudo docker login
 cd ~
 git clone https://github.com/kubeflow/manifests.git -b v1.8-branch
 
+# enable kubeflow to be accessed through https (1)
+cat << EOF >> ~/manifests/common/istio-1-16/kubeflow-istio-resources/base/kf-istio-resources.yaml
+    tls:
+      httpsRedirect: true
+  - hosts:
+    - '*'
+    port:
+      name: https
+      number: 443
+      protocol: HTTPS
+    tls:
+      mode: SIMPLE
+      privateKey: /etc/istio/ingressgateway-certs/tls.key
+      serverCertificate: /etc/istio/ingressgateway-certs/tls.crt
+EOF
+
+# enable kubeflow to be accessed through https (1)
+cat << EOF >> ~/manifests/common/istio-1-17/kubeflow-istio-resources/base/kf-istio-resources.yaml
+    tls:
+      httpsRedirect: true
+  - hosts:
+    - '*'
+    port:
+      name: https
+      number: 443
+      protocol: HTTPS
+    tls:
+      mode: SIMPLE
+      privateKey: /etc/istio/ingressgateway-certs/tls.key
+      serverCertificate: /etc/istio/ingressgateway-certs/tls.crt
+EOF
+
 # enable kubeflow to be accessed through http
-sed -i "s/true/false/g" ~/manifests/apps/jupyter/jupyter-web-app/upstream/base/params.env
-sed -i "s/true/false/g" ~/manifests/apps/volumes-web-app/upstream/base/params.env
-sed -i "s/true/false/g" ~/manifests/apps/tensorboard/tensorboards-web-app/upstream/base/params.env
+#sed -i "s/true/false/g" ~/manifests/apps/jupyter/jupyter-web-app/upstream/base/params.env
+#sed -i "s/true/false/g" ~/manifests/apps/volumes-web-app/upstream/base/params.env
+#sed -i "s/true/false/g" ~/manifests/apps/tensorboard/tensorboards-web-app/upstream/base/params.env
 
 # change service as nodeport
 sed -i "s/ClusterIP/NodePort/g" ~/manifests/common/dex/base/service.yaml
@@ -27,3 +59,7 @@ sudo mv ~/kustomize /usr/bin/
 # install kubeflow as a single command
 cd ~/manifests
 while ! kustomize build example | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 10; done
+
+# create certification for https connection
+sed -i 's/MASTER_IP/'"${MASTER_IP}"'/g' ${CURRENT_DIR}/certificate.yaml
+kubectl apply -f ${CURRENT_DIR}/certificate.yaml
